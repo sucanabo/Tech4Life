d39157d225091fd3a1b8b4fef1c7b67b470aad44
Phuc <sucanabo@gmail.com>
2020-11-26 10:56:04 +0700

    edit/folder_1055_2611

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,68 @@
+@extends('admin/layouts/master')
+@section('title','All Post - Index')
+@section('post-active','active')
+@section('content')
+<div class="table-responsive">
+    <table class="table">
+        <thead class="bg-primary text-white">
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                
+                <th>&nbsp;</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Nigam</td>
+                <td>Eichmann</td>
+                <td>@Sonu</td>
+                <td>Nigam</td>
+                <td>Eichmann</td>
+                <td>@Sonu</td>
+            
+                <td class="d-inline-block float-right">
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="fas fa-info-circle"></i> Details</button>
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="far fa-edit"></i> Edit</button>
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="fas fa-trash-alt"></i> Delete</button>
+                </td>
+                
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Deshmukh</td>
+                <td>Prohaska</td>
+                <td>@Genelia</td>
+                <td>Deshmukh</td>
+                <td>Prohaska</td>
+                <td>@Genelia</td>
+                <td class="d-inline-block float-right">
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="fas fa-info-circle"></i> Details</button>
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="far fa-edit"></i> Edit</button>
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="fas fa-trash-alt"></i> Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Roshan</td>
+                <td>Rogahn</td>
+                <td>@Hritik</td>
+                <td>Roshan</td>
+                <td>Rogahn</td>
+                <td>@Hritik</td>
+                <td class="d-inline-block float-right">
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="fas fa-info-circle"></i> Details</button>
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="far fa-edit"></i> Edit</button>
+                    <button type="button" class="btn btn-primary btn-rounded btn-sm"><i class="fas fa-trash-alt"></i> Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix'=>'admin'],function(){
-    Route::get('', function () {
+    Route::get('/', function () {
         return view('admin/home/index');
     });
     Route::get('posts', function () {
