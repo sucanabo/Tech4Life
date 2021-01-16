3fe48f757f51859cabb7b2b75ac33449432b6659
xuanloc <xuanloc.hcm2018@gmail.com>
2021-01-16 22:38:43 +0700

    Update 17/1

--- database/migrations/2021_01_16_152137_update.php
+++ database/migrations/2021_01_16_152137_update.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UserFollow extends Migration
+class Update extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,9 @@ class UserFollow extends Migration
     public function up()
     {
         //
-       
+        Schema::table('post', function ($table) {
+            $table->dropColumn('vote');
+        });
     }
 
     /**
